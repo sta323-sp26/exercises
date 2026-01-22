f1bf3b5d6d83d5bbee50bd82b33de06839f964ab
Colin Rundel <rundel@gmail.com>
2026-01-22 14:40:18 -0500

    Class

---
+++ class_2026-01-22_fizzbuzz/R/fizzbuzz.R
@@ -0,0 +1,32 @@
+#' @title Fizzbuzz function
+#'
+#' @description
+#' A short description...
+#'
+#' @param x *Numeric* vector to use for the fizzbuzz function
+#'
+#' @returns Returns a *character* vector of values, "fizz", "buzz", or "fizzbuzz"
+#'
+#' @examples
+#' fizzbuzz(1:10)
+#' fizzbuzz(10:1)
+#'
+#' @export
+#'
+fizzbuzz = function(x) {
+  stopifnot(all(x>0))
+  stopifnot(all(is.finite(x)))
+  stopifnot(is.numeric(x))
+
+  dplyr::case_when(
+    x %% 3 == 0 & x %% 5 == 0 ~ "fizzbuzz",
+    x %% 3 == 0 ~ "fizz",
+    x %% 5 == 0 ~ "buzz",
+    TRUE ~ as.character(x)
+  )
+}
+
+hello = function(x) {
+  print(c("Hello,", x,"!"))
+}
+
